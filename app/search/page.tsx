aad33578b687a60ca29c7bf2d6740ddd11d51dce
Tijani Ibrahim <megatey97@gmail.com>
2023-01-27 00:57:43 +0100

    fixing build problems

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -2,15 +2,15 @@ import fetchNews from "../../lib/fetchNews"
 import NewsList from "../NewsList"
 
 type Props = {
-    searchParams: {term: string};
+    searchParams?: {term: string};
 }
 
 
 const SearchPage = async ({searchParams}: Props) => {
-    const news: NewsResponse = await fetchNews("general", searchParams.term, true)
+    const news: NewsResponse = await fetchNews("general", searchParams?.term, true)
   return (
     <div>
-        <h1 className="headerTitle">Search Results for {searchParams.term}</h1>
+        <h1 className="headerTitle">Search Results for {searchParams?.term}</h1>
         <NewsList news={news}/>
     </div>
   )
